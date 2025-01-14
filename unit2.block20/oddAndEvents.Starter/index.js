599faf11cfb45356442560326b7146115eb6f0cb
erina <ealejo@stanford.edu>
2025-01-14 15:25:36 -0800

    trying to avoid nesting

--- unit2.block20/oddAndEvents.Starter/index.js
+++ unit2.block20/oddAndEvents.Starter/index.js
@@ -1,1 +1,60 @@
 // TODO: this file! :)
+
+/*
+When the user clicks the "Add Number" button, 
+the number they entered into the input field is added to the number bank.
+
+The number bank is not changed if the user enters a non-numeric value
+
+The number bank displays all the numbers the user has entered.
+
+When the "Sort 1" button is clicked, the first number in the number bank 
+is removed and placed into either the odd or even category.
+
+When the "Sort All" button is clicked, all the numbers in the number bank 
+are moved into either the odd or even category.
+
+The numbers are placed into the correct bucket based 
+on whether they are odd or even.
+
+References to selected DOM elements are stored in variables.
+
+The event listeners are listening for appropriate events, such as `submit` and `click`.
+
+The numbers in the bank, odd category, and even category are stored in state.
+
+The DOM is accurately rendered to reflect the state.
+
+*/
+
+const form = document.querySelector("form")
+
+// let numberBank = []
+// let oddNumbers = []
+// let evenNumbers = []
+
+// const button = document.getElementById('#form button')
+// const input = document.getElementById('#form number')
+
+// button.addEventListener('click', () => {
+//     const 
+// }
+
+// ) 
+
+
+// buttonElem.addEventListener('click', () => {
+//     const oldText = buttonElem.innerText;
+//     return button.innerText = oldText === "ON" ? "OFF" : "ON";
+//   });
+
+// const inputNumber = document.getElementById("number")
+// console.log(inputNumber);
+
+
+
+// const form = document.querySelector("form")
+// form.addEventListener("submit" event)
+//     )
+
+//     const userInput 
