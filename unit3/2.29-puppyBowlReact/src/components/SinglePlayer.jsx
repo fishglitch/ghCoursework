f2a7cb6bb32f9f0ddcfa8d287efd559ccab51845
Liz Hoppstetter <liz.hoppstetter@fullstackacademy.com>
2025-02-02 22:11:36 -0600

    pair programming single player component with liz

--- unit3/2.29-puppyBowlReact/src/components/SinglePlayer.jsx
+++ unit3/2.29-puppyBowlReact/src/components/SinglePlayer.jsx
@@ -2,41 +2,43 @@
 
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom"; 
+import { useParams, useNavigate } from "react-router-dom";
 
-export default function SinglePlayer({setSelectedPlayer}) {
+export default function SinglePlayer() {
+  let navigate = useNavigate();
+  const cohortName = "2409-GHP-ET-WEB-PT";
+  const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
-    const {id} = useParams(); // get player id from url
-    const [playerDetails, setPlayerDetails] = useState(null);
+  const { id } = useParams(); // get player id from url
+  const [playerDetails, setPlayerDetails] = useState(null);
 
-    useEffect(()=> {
-        const fetchPlayerDetails = async () => {
-            const response = await fetch (`${API_URL}/players/${id}`);
-            const data = await response.json();
-            if (!response.ok) {
-                console.error(data.error || "cannot fetch player deets");
-                return;
-            }
-            setPlayerDetails(data.data.selectedPlayer);
-        }
-        fetchPlayerDetails();
-    }, [id]);
-    
+  useEffect(() => {
+    const fetchPlayerDetails = async () => {
+      const response = await fetch(`${API_URL}/players/${id}`);
+      if (!response.ok) {
+        console.error(json.error || "cannot fetch player deets");
+        return;
+      }
+      const json = await response.json();
+      setPlayerDetails(json.data.player);
+    };
+    fetchPlayerDetails();
+  }, []);
 
-
-    return (
-        <>
+  return (
+    <>
+      {
         <section>
-            <h3>{playerDetails.name}</h3>
-            <p>{playerDetails.breed}</p>
-            <p>Player ID: {playerDetails.id}</p>
-            <p>Team ID: {playerDetails.teamId}</p>
-            <img src={playerDetails.imageUrl} alt={playerDetails.name}/>
-            <button onClick={() => setSelectedPlayer(null)}>Back</button>
+          <h3>{playerDetails?.name}</h3>
+          <p>{playerDetails?.breed}</p>
+          <p>Player ID: {playerDetails?.id}</p>
+          <p>Team ID: {playerDetails?.teamId}</p>
+          <img src={playerDetails?.imageUrl} alt={playerDetails?.name} />
+          <button onClick={() => navigate("/")}>Back</button>
         </section>
-        </>
-    );
-
+      }
+    </>
+  );
 }
 
 /*
