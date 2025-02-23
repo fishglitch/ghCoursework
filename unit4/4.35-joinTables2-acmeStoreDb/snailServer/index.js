1cde974fe6d323c4e8f5c971a78e4e032ce41d6e
fishglitch <fishglitch@users.noreply.github.com>
2025-02-23 07:51:01 -0800

    maintenance rename folder

---
+++ unit4/4.35-joinTables2-acmeStoreDb/snailServer/index.js
@@ -0,0 +1,65 @@
+const db = require("./db");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/users", async (req, res, next) => {
+  try {
+    const result = await db.fetchUsers();
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/products", async (req, res, next) => {
+  try {
+    const result = await db.fetchProducts();
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /api/users/:id/favorites:
+// Has a product_id as the payload, and
+// returns the created favorite with a status code of 201.
+app.get("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    const result = await db.fetchFavorites();
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    const deleteFavoriteId = req.params.id;
+
+    // check if favorite exists before deleting
+    const existingFavorite = await db.fetchFavorites();
+    const favoriteExists = existingFavorite.some(
+      (favorite) => favorite.id === deleteFavoriteId
+    );
+
+    if (!favoriteExists) {
+      return res.status(404).json({ message: "Favorite not found" });
+    }
+
+    await db.destroyFavorite(deleteFavoriteId);
+    res.status(204).send();
+    console.log("deleted reservation", deleteFavoriteId);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+const init = async () => {
+  app.listen(3000, () => console.log("listening on port 3000"));
+  console.log("init api");
+  db.init();
+};
+
+init();
